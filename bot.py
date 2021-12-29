3fd47b3366552b3e39a0024adb809220071b91e7
Dmitry Katargin <d.katargin@corp.mail.ru>
2021-12-29 18:47:22 +0300

    extract noun and uniq words as tokens and return tags as reply;

--- bot.py
+++ bot.py
@@ -1,11 +1,36 @@
+import requests
+import validators
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from bs4 import BeautifulSoup
 from telegram.ext import Updater, Filters, MessageHandler
 
 from common import config, logger
 
+tokenizer = RegexpTokenizer(r'\w+')
 
 def process_data(text):
-    text_type = "text"
+    target_text = text.lower()
+    if validators.url(text.strip()):
+        req = requests.get(text)
+        content = req.content[:100000]
+        soup = BeautifulSoup(content, "html")
+        words = ""
+        html_title = soup.find("title")
+        og_descr = soup.find("meta", property="og:description")
+        twitter_descr = soup.find("meta", property="twitter:description")
+        if html_title and len(html_title.contents) != 0:
+            words += html_title.contents[0]
+        if og_descr and len(og_descr.contents) != 0:
+            words += og_descr.contents[0]
+        if twitter_descr and len(twitter_descr.contents) != 0:
+            words += twitter_descr.contents[0]
+        target_text = words
 
+    tokenized = tokenizer.tokenize(target_text.lower())
+    is_noun = lambda pos: pos[:2] == 'NN'
+    tags = set([word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)])
+    return tags
 
 
 def main(update, context):
@@ -13,7 +38,12 @@ def main(update, context):
     if update.message.chat.id != int(config.config.get("Telegram", "admin_id")):
         logger.bot_logger.warning(f"request from unauthorized user-id {update.message.chat.id}")
         return
-    process_data(data)
+    tags = process_data(data)
+    result = ""
+    for t in tags:
+        result += f"#{t} "
+
+    update.message.reply_text(result)
 
 
 if __name__ == "__main__":
